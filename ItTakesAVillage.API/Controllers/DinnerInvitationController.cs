54956c734084a8cf73eda381a84d7779ed5d562c
Maria Holster <maria.holster@outlook.com>
2024-04-19 11:19:44 +0200

    add controller files thar are needed

---
+++ ItTakesAVillage.API/Controllers/DinnerInvitationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ItTakesAVillage.API.Controllers
+{
+    public class DinnerInvitationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ItTakesAVillage.API/Controllers/PlayDateController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ItTakesAVillage.API.Controllers
+{
+    public class PlayDateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

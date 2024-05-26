9321af19f1efe4d617ad1903b265130f536bcdc1
Maria Holster <maria.holster@outlook.com>
2024-05-26 23:19:07 +0200

    add controller for user

---
+++ backend/ItTakesAVillage.API/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿namespace ItTakesAVillage.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(IItTakesAVillageUserService itTakesAVillageUserService) : ControllerBase
+    {
+        private readonly IItTakesAVillageUserService _itTakesAVillageUserService = itTakesAVillageUserService;
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            try
+            {
+                var user = await _itTakesAVillageUserService.GetAsync(id);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}

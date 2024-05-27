1b506c0c3bea164273caf10984217e1bef210010
Maria Holster <111920470+maryyj@users.noreply.github.com>
2024-05-27 11:24:20 +0200

    redirect to Indexpage (#86)

--- frontend/ItTakesAVillage.Frontend/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ frontend/ItTakesAVillage.Frontend/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -2,19 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using ItTakesAVillage.Frontend.Models;
+
 
 namespace ItTakesAVillage.Frontend.Areas.Identity.Pages.Account
 {
@@ -116,7 +105,7 @@ namespace ItTakesAVillage.Frontend.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
+                    return RedirectToPage("/Index");
                 }
                 if (result.RequiresTwoFactor)
                 {

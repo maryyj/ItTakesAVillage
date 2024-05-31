22a3bd562c0bb0633b513bee0061271da6bc0990
Maria Holster <maria.holster@outlook.com>
2024-05-31 11:48:19 +0200

    remove unncessary code

--- frontend/ItTakesAVillage.Frontend/Models/ItTakesAVillageUser.cs
+++ frontend/ItTakesAVillage.Frontend/Models/ItTakesAVillageUser.cs
@@ -10,9 +10,9 @@ public class ItTakesAVillageUser : IdentityUser
 
     public ICollection<UserGroup>? UserGroups { get; set; }
 
-    public static implicit operator ItTakesAVillageUser(Group v)
-    {
-        throw new NotImplementedException();
-    }
+    //public static implicit operator ItTakesAVillageUser(Group v)
+    //{
+    //    throw new NotImplementedException();
+    //}
 }
 
